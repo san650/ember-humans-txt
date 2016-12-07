c5ed6f8d417d78defd14cb2d65fdde1881160b57
Santiago Ferreira <san650@gmail.com>
2016-12-07 19:26:34 -0300

    Add acceptance test

---
+++ node-tests/acceptance/humans-txt-test.js
@@ -0,0 +1,29 @@
+var assert = require('assert');
+var RSVP = require('rsvp');
+var readFile = RSVP.denodeify(require('fs').readFile);
+var AddonTestApp = require('ember-cli-addon-tests').AddonTestApp;
+
+describe('Acceptance: humans.txt file generation', function() {
+  this.timeout(300000);
+
+  var app;
+
+  before(function() {
+    app = new AddonTestApp();
+
+    return app.create('dummy', {
+        fixturesPath: 'node-tests/acceptance/fixtures'
+      }).then(function() {
+        return app.runEmberCommand('generate', 'humans-txt');
+      }).then(function() {
+        return app.runEmberCommand('build');
+      });
+  });
+
+  it('generates a humans.txt file', function() {
+    return readFile(app.filePath('/dist/humans.txt'))
+      .then(function(content) {
+        assert.ok(content.includes('developer: Santiago'));
+      });
+  });
+});
