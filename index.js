574b3f346459d66e078f5112da62adf9811c1061
Santiago Ferreira <san650@gmail.com>
2016-12-07 18:40:07 -0300

    Generate humans.txt from configuration
    Adds a broccoli plugin for generating the new file humans.txt by
    interpreting the conf/humans.js configuration file.
    
    This commit doesn't include the transformation step yet, just the
    generation step.

--- index.js
+++ index.js
@@ -8,5 +8,14 @@ module.exports = {
     if (section === 'head') {
       return '<link rel="author" href="humans.txt">';
     }
+  },
+
+  treeForPublic: function() {
+    var path = require('path');
+    var MakeHumansTXT = require('./lib/broccoli/make-humans-txt');
+    var humans = require(path.join(
+      this.app.project.root, './config/humans.js'));
+
+    return new MakeHumansTXT('.', humans());
   }
 };
---
+++ node-tests/broccoli/make-humans-txt-test.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var testHelpers = require('broccoli-test-helpers');
+var makeTestHelper = testHelpers.makeTestHelper;
+var cleanupBuilders = testHelpers.cleanupBuilders;
+var assert = require('assert');
+var readFileSync = require('fs').readFileSync;
+var path = require('path');
+
+var MakeHumansTXT = require('../../lib/broccoli/make-humans-txt');
+
+describe('Broccoli: MakeHumansTXT', function() {
+  var MakeHumansTXTHelper = makeTestHelper({
+    fixturePath: __dirname,
+
+    subject: function(inputNode) {
+      return new MakeHumansTXT(inputNode, {
+        team: [
+          { title: 'developer', name: 'Santiago' }
+        ]
+      });
+    },
+  });
+
+  afterEach(function() {
+    return cleanupBuilders();
+  });
+
+  it('generates humans.txt file', function() {
+    return MakeHumansTXTHelper('fixtures')
+      .then(function(result) {
+        assert.deepEqual(result.files, ['humans.txt']);
+        return path.join(result.directory, result.files[0]);
+      })
+      .then(readFileSync)
+      .then(function(content) {
+        assert.ok(content.includes('developer: Santiago'));
+      });
+  });
+});
